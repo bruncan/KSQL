2bc556c08c67d9399262f0b35f637b641d448509
Bruno Candia <bruncan6@outlook.com>
2023-08-20 14:17:55 -0400

    Query 1 ajustado y funcionando

--- Q1.sql
+++ Q1.sql
@@ -1,7 +1,9 @@
-SELECT
-    symbol,
-    SUM(price * volume) / SUM(volume) AS promedio_ponderado
-FROM
-    finnhub_trades_stream
-GROUP BY
-    symbol EMIT CHANGES;
+Create table promedio_ponderado2 AS
+    SELECT
+        symbol,
+        SUM(price) / Count(*) AS promedio_ponderado
+    FROM
+        finnhub_trades_stream
+    GROUP BY
+        symbol
+    EMIT CHANGES;
