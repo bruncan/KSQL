ae771793e9576d7fab0e6cc8ee416bdc93615372
Bruno Candia <bruncan6@outlook.com>
2023-08-20 19:08:02 -0400

    Pasos comentados

--- Querys.sql
+++ Querys.sql
@@ -1,3 +1,12 @@
+--Carga el archivo de docker-compose.yml
+docker-compose up -d
+
+--Inicia ksqlDB
+docker exec -it ksqldb-cli ksql http://ksqldb-server:8088
+
+--Da de baja el docker
+docker-compose down
+
 --Se crea el Stream para almacenar los resultados del Consumer en KSQL
 
 CREATE STREAM finnhub_trades_stream (
@@ -13,13 +22,12 @@ CREATE STREAM finnhub_trades_stream (
 --1. ¿Cuál fue el promedio ponderado de precio de una unidad por cada uno de los símbolos procesados? (e.j. AAPL)
 --Se crea una tabla auxiliar para el calculo del promedio
 CREATE TABLE promedio_ponderado AS
-    SELECT
+    SELECT 
         symbol,
-        SUM(price) / Count(*) AS promedio_ponderado
-    FROM
-        finnhub_trades_stream
-    GROUP BY
-        symbol
+        SUM(price) / SUM(volume) AS promedio_ponderado_precio
+    FROM finnhub_trades_stream
+    WHERE price IS NOT NULL AND volume IS NOT NULL
+    GROUP BY symbol
     EMIT CHANGES;
 
 --Consulta para obtener los resultados de la tabla
